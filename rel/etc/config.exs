5a708ac01f16239e1134bff46513e8619badc522
Viktor Nareiko <vnareiko.lt@gmail.com>
2021-01-08 08:08:13 +0200

    cluster connection

--- rel/etc/config.exs
+++ rel/etc/config.exs
@@ -44,13 +44,32 @@ config :distillery_example, ExampleWeb.Endpoint,
   root: ".",
   secret_key_base: "u1QXlca4XEZKb1o3HL/aUlznI1qstCNAQ6yme/lFbFIs0Iqiq/annZ+Ty8JyUCDc"
 
+# config :libcluster,
+#   topologies: [
+#     example: [
+#       strategy: ClusterEC2.Strategy.Tags,
+#       config: [
+#         ec2_tagname: "Name",
+#         ec2_tagvalue: "#{app}-#{env}"
+#       ]
+#     ]
+#   ]
+
 config :libcluster,
   topologies: [
     example: [
-      strategy: ClusterEC2.Strategy.Tags,
-      config: [
-        ec2_tagname: "Name",
-        ec2_tagvalue: "#{app}-#{env}"
-      ]
+      # The selected clustering strategy. Required.
+      strategy: Cluster.Strategy.Epmd,
+      # Configuration for the provided strategy. Optional.
+      config: [hosts: [:"a@10.0.0.242", :"b@10.0.1.205"]],
+      # The function to use for connecting nodes. The node
+      # name will be appended to the argument list. Optional
+      connect: {:net_kernel, :connect_node, []},
+      # The function to use for disconnecting nodes. The node
+      # name will be appended to the argument list. Optional
+      disconnect: {:erlang, :disconnect_node, []},
+      # The function to use for listing nodes.
+      # This function must return a list of node names. Optional
+      list_nodes: {:erlang, :nodes, [:connected]},
     ]
   ]
